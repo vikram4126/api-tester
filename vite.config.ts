1bf671e46b5424aab1fcbc9d214e731ec7a1b570
vikram4126 <vikram.aptara@gmail.com>
2026-05-03 13:02:38 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 5173,
+    proxy: {
+      '/proxy': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      }
+    }
+  }
 })

4e641f873d3e74d01d48cb6a3d92fa792c6c5aea
vikram4126 <vikram.aptara@gmail.com>
2026-05-04 14:34:14 +0530

    add dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
